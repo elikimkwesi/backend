b5f1f453d3cbcd4c1152d8e2fac7c3f29972e714
Sylvester Amoah <kojoamoah04@gmail.com>
2024-08-07 17:31:41 +0000

    Merged with unrelated histories

--- models/user.js
+++ models/user.js
@@ -10,8 +10,4 @@ const UserSchema = new Schema({
 
 const User = mongoose.model('User', UserSchema);
 
-<<<<<<< HEAD
 module.exports = User;
-=======
-module.exports = User;
->>>>>>> b913c834adfa81a4bca77b45bea2ec3c6df5a445
